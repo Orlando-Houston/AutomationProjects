9bcdda3e4acc87c4075187caebcc3815c9acb199
Orlando-Houston <54649393+Orlando-Houston@users.noreply.github.com>
2021-04-20 18:47:24 -0400

    webOrdersLoginPage

--- Driver.java
+++ Driver.java
@@ -1,39 +1,19 @@
-package NauWebProject;
+package Common;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
-
 public class Driver {
-    private static WebDriver driver;
-    public  static WebDriver getDriver(){
-        if (driver==null){
-            setDriver();
-        }
-        return driver;
-    }
-    public static void setDriver(){
-        if (driver==null){
-            WebDriverManager.chromedriver ().setup ();
-            driver=new ChromeDriver ();
-        }
-    }
-    public static void closeDriver(){
-        if (driver!=null){
-            driver.quit ();
-            driver=null;
-        }
-    }
-    public static String getProp(String url) throws IOException {
-        Properties prop=new Properties ();
-        FileInputStream fis=new FileInputStream ("src\\test\\java\\NauWebProject\\global.properties");
-        prop.load (fis);
-        return prop.getProperty (url);
-    }
+    //public static WebDriver driver;
 
-}
+    public static WebDriver getDriver(){
+
+            WebDriverManager.chromedriver ().setup ();
+            WebDriver driver=new ChromeDriver ();
+            return driver;
+        }
+
+        }
+
+
--- LoginTest.java
+++ LoginTest.java
@@ -1,44 +1,42 @@
 package LoginTest;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import Common.Driver;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import static Common.Driver.getDriver;
 
 public class LoginTest {
+
     @Test
-    public void verifyUserLoginWithCorrectCredentials(){
-        WebDriverManager.chromedriver ().setup ();
-        WebDriver driver=new ChromeDriver ();
-        driver.get ("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
+    public void verifyUserLoginWithCorrectCredentials() throws InterruptedException {
+       Driver. getDriver ().get ("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/login.aspx");
+        //driver.manage ().window ().maximize ();
+       // driver.manage ().timeouts ().implicitlyWait (10, TimeUnit.SECONDS);
+        Thread.sleep (3000);
 
-        driver.findElement (By.id ("ctl00_MainContent_username")).sendKeys ("Tester");
-        driver.findElement (By.id ("ctl00_MainContent_password")).sendKeys ("test");
-        driver.findElement (By.id ("ctl00_MainContent_login_button")).click ();
+            //methods name
+        Driver. getDriver ().findElement (By.id ("ctl00_MainContent_username")).sendKeys ("Tester");
+        Driver.getDriver ().findElement (By.id ("ctl00_MainContent_password")).sendKeys ("test");
+       Driver. getDriver ().findElement (By.id ("ctl00_MainContent_login_button")).click ();
         Assert.assertEquals ("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/default.aspx",
-                driver.getCurrentUrl ());
-
+                getDriver ().getCurrentUrl ());
     }
     @Test
     public void verifyUserLoginWithInCorrectPasswordIsErrorMessage(){
-        WebDriverManager.chromedriver ().setup ();
-        WebDriver driver=new ChromeDriver ();
-        driver.get ("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
+       Driver. getDriver ().get ("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
 
-        driver.findElement (By.id ("ctl00_MainContent_username")).sendKeys ("Tester");
-        driver.findElement (By.id ("ctl00_MainContent_password")).sendKeys ("asdfg");
-        driver.findElement (By.id ("ctl00_MainContent_login_button")).click ();
+       Driver. getDriver ().findElement (By.id ("ctl00_MainContent_username")).sendKeys ("Tester");
+       Driver. getDriver ().findElement (By.id ("ctl00_MainContent_password")).sendKeys ("asdfg");
+        Driver.getDriver ().findElement (By.id ("ctl00_MainContent_login_button")).click ();
         //String errorMessageText=driver.findElement (By.id ("ctl00_MainContent_status")).getText ();
-       // Assert.assertEquals (errorMessageText,"invalid login or password");
+        // Assert.assertEquals (errorMessageText,"invalid login or password");
         Assert.assertEquals ("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/login.aspx",
-                driver.getCurrentUrl ());
-
-
-
+                getDriver ().getCurrentUrl ());
 
     }
-
 
 }
