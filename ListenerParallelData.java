ce3ec50522d346c64fe22487e86ad37d3fbd94ae
Orlando-Houston <54649393+Orlando-Houston@users.noreply.github.com>
2021-04-24 17:02:24 -0400

    parallelExcecution

---
+++ ListenerParallelData.java
@@ -0,0 +1,35 @@
+package ParallelExecutionTursday;
+
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ListenerParallelData {
+
+    @BeforeClass
+    public void  setThread(ITestContext con){
+        con.getCurrentXmlTest().getSuite().setDataProviderThreadCount(3);
+    }
+     @Test(dataProvider = "DataSource")
+    public void  test(int i,String y) {
+         long id = Thread.currentThread().getId();
+
+         System.out.println(i + "-" + y + "has been executed in thread number:" + id);
+     }
+    @DataProvider(name = "DataSource", parallel = true)
+    public Object[][] getData () {
+        return new Object[][]{   //new object[][1]
+                {1, "a"},
+                {2, "b"},
+                {3, "c"},
+                {4, "d"},
+                {5, "e"},
+                {6, "f"},
+                {7, "g"},
+                {8, "h"},
+                {9, "i"},
+                {10, "j"},
+        };
+    }
+}
