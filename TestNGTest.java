97ca0d29ac2374ac5c19827241c6e1b4534f557d
Orlando-Houston <54649393+Orlando-Houston@users.noreply.github.com>
2021-04-24 17:06:36 -0400

    TNG

---
+++ TestNGTest.java
@@ -0,0 +1,19 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+
+public class TestNGTest {
+    @Test
+    void testCase1(){
+        System.out.println("Hello everyone");
+    }
+    @Test
+    void testCase2(){
+        System.out.println("Hello");
+    }
+    @Test
+    void testCase3(){
+        System.out.println("everyone");
+    }
+}
